5281a08a25d816cb6e00eb2594cef00c4a4f73f6
Jose A Martin <j.martin.bejarano@gmail.com>
2014-08-03 18:59:30 +0200

    set a grain which contains the HOME variable

--- config.sh
+++ config.sh
@@ -3,5 +3,8 @@
 # replace template with my own user's home
 sed "s#\${home}#$HOME#" salt/minion.tpl > salt/minion
 
+# we set home as a grain
+sudo salt-call -c ~/code/config/salt --local grains.setval home $HOME
+
 # call high state on the minion
 sudo salt-call -c ~/code/config/salt --local state.highstate
